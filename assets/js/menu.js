4deb8972aab970332548832860ea6d355185f54a
antoniocarlosnego <antoniocoliver@usp.br>
2026-05-19 12:05:45 -0300

    feat: blog

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -17,6 +17,7 @@ const renderNavbar = () => {
             <ul class="bem-navbar__menu">
                 <li class="bem-navbar__item"><a href="#servicos" class="bem-navbar__link">Áreas de Atuação</a></li>
                 <li class="bem-navbar__item"><a href="#sobre" class="bem-navbar__link">Sobre</a></li>
+                <li class="bem-navbar__item"><a href="#blog" class="bem-navbar__link">Blog</a></li>
                 <li class="bem-navbar__item"><a href="#faq" class="bem-navbar__link">Dúvidas</a></li>
                 <li class="bem-navbar__item"><a href="#contato" class="bem-btn bem-btn--primary">Consultoria</a></li>
             </ul>
